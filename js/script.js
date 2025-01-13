0c74231177a868683e91afc5b1cf86f743d6c09e
Lucarim <lucas--amorim@outlook.com>
2025-01-13 20:52:08 -0300

    menu ativo

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link) {
+  if (location.href.includes(link.href)) {
+    link.classList.add("ativo");
+  }
+}
+links.forEach(ativarLink);
