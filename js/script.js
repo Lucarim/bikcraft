5524aec3cae778cec65065d86b9b3ed64367816d
Lucarim <lucas--amorim@outlook.com>
2025-01-15 21:16:01 -0300

    parametros url

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
+// Ativar links do menu
 const links = document.querySelectorAll(".header-menu a");
-
 function ativarLink(link) {
   if (location.href.includes(link.href)) {
     link.classList.add("ativo");
   }
 }
 links.forEach(ativarLink);
+
+// Ativar itens do orçamento
+
+const parametros = new URLSearchParams(location.search);
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+parametros.forEach(ativarProduto);
